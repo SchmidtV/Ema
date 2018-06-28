0193cb6d790952dd72a2be1bbab27991d97773ff
VSchmidt <vladimir_schmidt@hotmail.com>
2018-06-29 00:37:28 +0200

    nav

--- App.js
+++ App.js
@@ -1,73 +1,13 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
-import PlaceList from "./src/components/PlaceList";
-import PlaceInput from "./src/components/PlaceInput";
-import PlaceDetail from "./src/components/PlaceDetail";
-import {addPlace, deletePlace, selectPlace, deselectPlace} from "./src/store/actions/index";
-import {connect} from "react-redux";
+import {Navigation} from "react-native-navigation";
+import AuthScreen from "./src/screens/Auth/Auth";
 
-class App extends Component {
+//register screens
+Navigation.registerComponent("ema.AuthScreen", () => AuthScreen);
 
-    placeSubmitHandler = (placeName) => {
-        this.props.onAddPlace(placeName);
-    };
-
-    onItemDeleted = () => {
-        this.props.onDeletePlace();
-
-    };
-    onItemSelected = (key) => {
-        this.props.onSelectPlace(key);
-    };
-
-    onModalClose = () => {
-        this.props.onDeselectPlace();
-    };
-
-    render() {
-
-        return (
-            <View style={styles.container}>
-                <PlaceDetail
-                    selectedPlace={this.props.selectedPlace}
-                    onItemDeleted={this.onItemDeleted}
-                    onModalClose={this.onModalClose}
-                />
-                <PlaceInput placeSubmitHandler={this.placeSubmitHandler}/>
-                <PlaceList
-                    places={this.props.places}
-                    onItemSelected={this.onItemSelected}/>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    listContainer: {
-        width: "100%"
+//start app
+Navigation.startSingleScreenApp({
+    screen: {
+        screen: "ema.AuthScreen",
+        title: "Login"
     }
 });
-
-const mapStateToProps = state => {
-    return {
-        places: state.places.places,
-        selectedPlace: state.places.selectedPlace
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddPlace: (name) => dispatch(addPlace(name)),
-        onDeletePlace: () => dispatch(deletePlace()),
-        onSelectPlace: (key) => dispatch(selectPlace(key)),
-        onDeselectPlace: () => dispatch(deselectPlace())
-    };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- android/app/src/main/java/com/ema/MainApplication.java
+++ android/app/src/main/java/com/ema/MainApplication.java
@@ -10,8 +10,34 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+import com.reactnativenavigation.NavigationApplication;
 
-public class MainApplication extends Application implements ReactApplication {
+
+public class MainApplication extends NavigationApplication  {
+
+    @Override
+    public boolean isDebug() {
+        // Make sure you are using BuildConfig from your own application
+        return BuildConfig.DEBUG;
+    }
+
+    protected List<ReactPackage> getPackages() {
+        // Add additional packages you require here
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+                // eg. new VectorIconsPackage()
+        );
+    }
+
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
+
+    @Override
+    public String getJSMainModuleName() {
+        return "index";
+    }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
